874560a2c41bbed729dc0dabc7a4d1e4b655d559
Alejandro Correa Arias <alejoariascorrea@gmail.com>
2024-06-27 23:11:07 -0500

    Initial commit

---
+++ src/main/java/ACA/TiendaLibros/Modelo/Libro.java
@@ -0,0 +1,26 @@
+package ACA.TiendaLibros.Modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idLibro;
+    private String titulo;
+    private String autor;
+    private Double precio;
+    private Integer existencias;
+}
---
+++ src/test/java/ACA/TiendaLibros/TiendaLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package ACA.TiendaLibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
